513ede207a12ef8428222d12922e8dcc89daa1cc
Sam <contactsamie@gmail.com>
2016-05-08 16:35:41 -0400

    testing multiple continuations

--- SuperposeLib.Tests/job_with_command.cs
+++ SuperposeLib.Tests/job_with_command.cs
@@ -27,6 +27,46 @@ namespace SuperposeLib.Tests
         }
 
 
+
+        [TestMethod]
+        public void test_using_owin_mulitple_continuation_job()
+        {
+            const string baseAddress = "http://*:8118/";
+            using (WebApp.Start<TestStartup>(new StartOptions(baseAddress)))
+            {
+                Console.WriteLine("Server started");
+                var command1 = new TestCommand() { MyName = "tester1" };
+                var command2 = new TestCommand() { MyName = "tester2" };
+                var command3 = new TestCommand() { MyName = "tester3" };
+                var command4 = new TestCommand() { MyName = "tester4" };
+
+
+                var jobId = JobHandler.EnqueueJob<JobWithCommand, TestCommand>(command1,
+                            (continuation2) => continuation2.EnqueueJob<JobWithCommand, TestCommand>(command2,
+                             (continuation3) => continuation3.EnqueueJob<JobWithCommand, TestCommand>(command2,
+                              (continuation4) => continuation4.EnqueueJob<JobWithCommand, TestCommand>(command2)
+                             )
+                            )
+                           );
+
+                AssertAwait(() => EnsureJobHasRun(jobId), 5000);
+
+                var storage = SuperposeGlobalConfiguration.StorageFactory.CreateJobStorage();
+                var converter = SuperposeGlobalConfiguration.JobConverterFactory.CretateConverter();
+
+                IJobFactory factory = new JobFactory(storage, converter);
+                var statistics = factory.JobStorage.JobLoader.GetJobStatistics();
+                Assert.AreEqual(statistics.TotalNumberOfJobs, 4);
+                Assert.AreEqual(statistics.TotalSuccessfullJobs, 4);
+                Assert.AreEqual(statistics.TotalFailedJobs, 0);
+                Assert.AreEqual(statistics.TotalProcessingJobs, 0);
+            }
+        }
+
+
+
+
+
         [TestMethod]
         public void test_using_owin_continuation_job()
         {
